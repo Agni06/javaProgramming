36f06db1ed7c84d0ad7c99f9e8fc96f7298ed0fe
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-01-01 15:29:25 +0530

    Create calanderObject.java

---
+++ calanderObject.java
@@ -0,0 +1,14 @@
+class test
+  {
+    public static void main(String, args[])
+    {
+
+      java.util.Calendar current;
+
+      current  = java.util.Calendar.getInstance();
+
+      year= current.get(java.util.Calendar.YEAR);
+
+      System.out.println("Current Year: "+year);
+    }
+  }
