0337a755c22dfc5cd7a85a6f7b75a5794c4bd2c8
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-01-01 15:23:08 +0530

    Create PRINT'NPTEL JAVA'.java

---
+++ PRINT'NPTEL JAVA'.java
@@ -0,0 +1,13 @@
+class test
+  {
+    public static void main(String[] args)
+    {
+      String nptel,space,java;
+
+      nptel="NPTEL";
+	space=" ";
+	java="JAVA";
+     	System.out.print(nptel+space+java);
+    }
+  }
+
