b18995689bd06915b3122e35660a617c9eb12ae7
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-01-01 15:25:46 +0530

    Create importingPackages.java

---
+++ importingPackages.java
@@ -0,0 +1,16 @@
+import static java.lang.System.out;
+import java.util.Scanner;
+
+
+class test
+  {
+
+    public static void main(String, args)
+    {
+
+      Scanner sc = new Scanner(System.in);
+       String courseName = scanner.nextLine(); 
+
+      System.out.println(courseName);
+    }
+  }
