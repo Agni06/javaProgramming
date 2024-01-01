daee8f60dad4bddefb7cbb867860dd425f76f979
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-01-01 14:45:19 +0530

    Create callingMethodsInsideAnotherClass.java

---
+++ callingMethodsInsideAnotherClass.java
@@ -0,0 +1,23 @@
+class main
+  {
+
+    public static void main(String,args)
+    {
+
+      main obj  = new main();
+      
+    }
+
+    void print(main obj)
+    {
+
+      System.out.print("Well Done!");
+    }
+    void studentMethod()
+    {
+
+      main obj  = new main();
+      obj.print(obj);
+      
+    }
+  }
